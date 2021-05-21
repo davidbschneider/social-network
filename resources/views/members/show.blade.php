78f865b9895d3341f1938cba1dbde6f4cda8c504
davidbschneider <davidschneider@protonmail.com>
2021-05-21 13:18:13 +0200

    Using MemberCard for member page

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -1,17 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="h4 font-weight-bold">
-            <div class="d-flex">
-                <div class="flex-shrink-0">
-                    <img class="rounded-circle" src="{{ $member->profile_photo_url }}" alt="{{ $member->name }}">
-                </div>
-                <div class="flex-grow-1 ms-3">
-                    <h2 class="">
-                        {{ $member->name }}
-                    </h2>
-                </div>
-            </div>
-        </h2>
+        <x-member-card :member="$member"/>
     </x-slot>
     <div class="container">
         <div class="row">
